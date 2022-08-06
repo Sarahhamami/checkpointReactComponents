beb0f9b4e9000714c3d7a6533e6702aaacd875f6
Sarahhamami <92446749+Sarahhamami@users.noreply.github.com>
2022-08-06 09:13:56 +0100

    Update ProfilePhoto.js

--- src/Component/Profile/ProfilePhoto.js
+++ src/Component/Profile/ProfilePhoto.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react'
 
 
-export class ProfilePhoto extends Component {
-  render() {
-    return (<>
-            <img src="/ProfilePic.jpg" alt="myimage" style={{width: "100px",
-        height: "100px",
+import React from 'react'
+
+export default function ProfilePhoto() {
+  return (
+    
+    <img src="/profilePic.jpg" alt="myimage" style={{width: "300px",
+        height: "300px",
         'borderRadius':'50%',
         resizeMode: 'contain',
     alignSelf: 'center',}} />
-        </>
-    )
-  }
+        
+  )
 }
-
-export default ProfilePhoto
